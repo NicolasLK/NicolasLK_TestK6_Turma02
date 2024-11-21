cfb3fd115b098210f70679dde40663e309b747c5
Nicolas Loffi Kaminski <nicolas.38720@alunosatc.edu.br>
2024-11-21 20:36:17 -0300

    fix: Adicionando biblioteca faltando

--- src/tests/Tests.spec.js
+++ src/tests/Tests.spec.js
@@ -1,4 +1,5 @@
 import { htmlReport } from 'https://raw.githubusercontent.com/benc-uk/k6-reporter/main/dist/bundle.js';
+import { textSummary } from 'https://jslib.k6.io/k6-summary/0.0.1/index.js';
 import http from 'k6/http';
 import { check, sleep } from 'k6';
 import { Trend } from 'k6/metrics';
@@ -15,8 +16,8 @@ export const options = {
 
 export function handleSummary(data) {
   return {
-    './src/output/index.html': htmlReport(data)
-    // stdout: textSummary(data, { indent: ' ', enableColors: true })
+    './src/output/index.html': htmlReport(data),
+    stdout: textSummary(data, { indent: ' ', enableColors: true })
   };
 }
 
